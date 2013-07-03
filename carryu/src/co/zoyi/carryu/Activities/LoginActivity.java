12334a8830a9bd4ae9888be2641a97d7e47ed741
Allen Jee <walsub@gmail.com>
2013-07-03 15:20:04 +0900

    Add login activity.

---
+++ carryu/src/co/zoyi/carryu/Activities/LoginActivity.java
@@ -0,0 +1,13 @@
+package co.zoyi.carryu.Activities;
+
+import android.os.Bundle;
+import co.zoyi.carryu.Activities.CUActivity;
+import co.zoyi.carryu.R;
+
+public class LoginActivity extends CUActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
